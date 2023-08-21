7f13d8a93933455b03fa557484df92116fa0ee76
unknown <robertgicheha3@gmail.com>
2023-08-22 01:49:17 +0300

    worked on Registering a new user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,7 @@
 const asyncHandler = require('express-async-handler')
+const bcrypt = require('bcrypt.js')
+
+const User = require('../models/userModel')
 
 //@Desc Register new user
 //@router /api/users
@@ -13,7 +16,39 @@ const registerUser = asyncHandler(async (req, res) => {
     res.status(400)
     throw new Error('Please include all the fields')
   }
-  res.send('Register Route')
+
+  //Find if user already exists
+  const userExists = await User.findOne({ email })
+
+  if (userExists) {
+    res.status(400)
+
+    throw new Error('User Already exists')
+  }
+
+  //Hash Password
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(password, salt)
+
+  //Create User
+
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  })
+
+  if(user){
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email
+    })
+
+  }else{
+    res.status(400)
+    throw new Error('Invalid User Data')
+  }
 })
 
 //@Desc Login user
