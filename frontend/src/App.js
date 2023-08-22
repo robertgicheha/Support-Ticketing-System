3994cc1ed21c2a4f2329ed2c6c66096bc7e232b9
unknown <robertgicheha3@gmail.com>
2023-08-22 17:38:56 +0300

    worked on the logout functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import Header from './components/Header'
 import Home from './pages/Home'
 import Register from './pages/Register'
 import Login from './pages/Login'
-import LogOut from './pages/LogOut'
+
 function App() {
   return (
     <>
@@ -16,7 +16,6 @@ function App() {
             <Route path='/' element={<Home />} />
             <Route path='/login' element={<Login />} />
             <Route path='/register' element={<Register />} />
-            <Route path='/logout' element={<LogOut />} />
           </Routes>
         </div>
       </Router>
