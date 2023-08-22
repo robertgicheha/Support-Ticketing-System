ceb1656e5cb106a9d1c4dfb87f3486b4b578e39a
unknown <robertgicheha3@gmail.com>
2023-08-22 14:13:37 +0300

    Home,Login and Register UI

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,10 +1,65 @@
-import React from 'react'
+import { useState } from 'react'
+import { FaSignInAlt } from 'react-icons/fa'
 
 function Login() {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  })
+
+  const { email, password } = formData
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }))
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+  }
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <>
+      <section className='heading'>
+        <h1>
+          <FaSignInAlt /> Login
+        </h1>
+        <p>Please Login to your account</p>
+      </section>
+
+      <section className='form'>
+        <form onSubmit={onSubmit}>
+          <div className='form-group'>
+            <input
+              type='email'
+              className='form-control'
+              id='email'
+              name='email'
+              value={email}
+              onChange={onChange}
+              placeholder='Enter your email'
+              required
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='password'
+              className='form-control'
+              id='password'
+              name='password'
+              value={password}
+              onChange={onChange}
+              placeholder='Enter your password'
+              required
+            />
+          </div>
+          <div className='form-group'>
+            <button className='btn btn-block'>LogIn</button>
+          </div>
+        </form>
+      </section>
+    </>
   )
 }
 
