818b0d5f2a9b7446b9761cccc5b2a788c8a0bd28
unknown <robertgicheha3@gmail.com>
2023-08-22 13:29:59 +0300

    Worked on Header and Components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,25 @@
-import './App.css'
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Home from './pages/Home'
+import Register from './pages/Register'
+import Login from './pages/Login'
+import LogOut from './pages/LogOut'
 function App() {
-  return <div>My App</div>
+  return (
+    <>
+      <Router>
+        <div className='container'>
+          <Header />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+            <Route path='/logout' element={<LogOut />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
+  )
 }
 
 export default App
